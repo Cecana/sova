46d683daf839943820295625090599e0460daa9b
Cecana <cecanam@gmail.com>
2016-10-21 21:34:25 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,321 @@
+
+/*-----------------------------------------------------------------------------
+  REQUIRE
+-----------------------------------------------------------------------------*/
+var yo = require('yo-yo')
+var minixhr = require('minixhr')
+var csjs = require('csjs-inject')
+/*-----------------------------------------------------------------------------
+  THEME
+-----------------------------------------------------------------------------*/
+var FONT        = 'Ubuntu, sans-serif'
+var BLACK       = 'hsla(240,5%,10%,1)'
+var WHITE       = 'hsla(200,20%,90%,1)'
+var BLUE        = 'hsla(191,64%,40%, 1)'
+var PINK        = 'hsla(330194%,71%, 1)'
+var PINKISH     = 'hsla(338,13%,72%, 1)'
+var BLUISH      = 'hsla(193,25%,63%,1)'
+var GREY        = 'hsla(30,100%,99%,1)'
+var LIGHTGREY   = 'hsla(30,100%,99%,4)'
+var COLORS      = [BLUISH, PINK, PINKISH, BLUE]
+/*-----------------------------------------------------------------------------
+  LOADING DATA
+-----------------------------------------------------------------------------*/
+minixhr('https://api.github.com/users/Cecana', responseHandler1)
+function responseHandler1 (data) {
+  document.body.appendChild(webpage({
+    data: JSON.parse(data),
+    theme: {
+       font: FONT,
+      black: BLACK,
+      white: WHITE,
+      lightGrey: LIGHTGREY,
+      blue: BLUE,
+      pink: PINK,
+      pinkish: PINKISH,
+      bluish: BLUISH,
+      grey: GREY,
+      colors: COLORS
+    }
+  }))
+  activateScrollEffect({ colors: COLORS })
+}
+/*-----------------------------------------------------------------------------
+  LOADING FONT
+-----------------------------------------------------------------------------*/
+var links = [
+  'https://fonts.googleapis.com/css?family=Ubuntu',
+  'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'
+]
+var font = yo`<link href=${links[0]} rel='stylesheet' type='text/css'>`
+var fontAwesome = yo`<link href=${links[1]} rel='stylesheet' type='text/css'>`
+document.head.appendChild(font)
+document.head.appendChild(fontAwesome)
+/*-----------------------------------------------------------------------------
+  WEB PAGE
+-----------------------------------------------------------------------------*/
+function webpage ({ data = {}, theme = {} } = {}) {
+  var css = csjs`
+    body {
+      text-align: center;
+      background-color: ${theme.bluish};
+      color: ${theme.black};
+      font-family: ${theme.font};
+    }
+    h1 {
+      margin-top: 1em;
+      color: ${theme.grey};
+      font-size: 4em;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  	h3 {
+      color: ${theme.grey};
+      font-size: 3em;
+      margin-bottom: 3em;
+    }
+    img {
+      margin-top: 3em;
+      border: 5px solid ${theme.grey};
+      border-radius: 50%;
+      width: 15em;
+    }
+  	@-webkit-keyframes bounce {
+      0% {
+        bottom: 50px;
+      }
+      70% {
+        bottom: 100px;
+        color: ${theme.lightGrey};
+      }
+      100% {
+        bottom: 50px;
+      }
+    }
+    .arrow {
+      position: relative;
+      font-size: 3em;
+      color: ${theme.grey};
+      animation: bounce 2s infinite
+    }
+  `
+  function template () {
+  	return yo`
+      <div>
+      <img src="${data.avatar_url}">
+        <h1>${data.name}</h1>
+        <h3>${data.bio}</h3>
+        <h5>${data.blog}</h5>
+        <div>
+        <i class="${css.arrow} fa fa-arrow-circle-down" aria-hidden="true"></i>
+
+        </div>
+        ${portfolioComponent({ theme: theme })}
+    		${textboxComponent({ theme: theme })}
+    		${footerComponent({ theme: theme })}
+      </div>
+    `
+  }
+  var element = template()
+  return element
+}
+/*--------------------------------------------------------------------------------
+  PORTFOLIO COMPONENT
+--------------------------------------------------------------------------------*/
+function portfolioComponent ({ data = {}, theme = {} } = {}) {
+	var css = csjs`
+  	.portfolio {
+      margin   : 2em 0 2em 0;
+      width    : 100%;
+    }
+    .portfolioItem     {
+      width            : 100%;
+      padding-bottom   : 200px;
+    	background-color : ${theme.blue};
+      color            : ${theme.grey};
+      display          : flex;
+      flex-direction   : column;
+      align-items      : flex-start;
+      transition       : 2s;
+    }
+    .portfolioItem_isHover {
+      width                : 100%;
+      padding-bottom       : 200px;
+    	background-color     : ${theme.bluish};
+      color                : ${theme.grey};
+      display              : flex;
+      flex-direction       : column;
+      align-items          : flex-start;
+      cursor               : pointer;
+      transition           : 2s;
+    }
+    .portfolioTitle    {
+      margin           : 2em;
+      padding          : 0.5em;
+      font-size        : 3em;
+      color            : ${theme.pink};
+      background-color : ${theme.pinkish};
+      border-radius    : 4px;
+      border           : 4px solid ${theme.pinkish};
+      transition       : 2s;
+    }
+    .portfolioTitle_isHover {
+      margin                : 2em 2em 2em 1.5em;
+      padding               : 0.5em;
+      font-size             : 3em;
+      color                 : ${theme.pink};
+      background-color      : ${theme.bluish};
+      border-radius         : 4px;
+      border                : 4px solid ${theme.grey};
+      transition            : 2s;
+    }
+    .portfolioBody {
+      margin       : 0 40% 0 0em;
+      text-align   : left;
+      font-size    : 1.2em;
+      color        : ${theme.blue};
+      transition   : 2s;
+    }
+    .portfolioBody_isHover {
+      margin               : 0 35% 0 4em;
+      text-align           : left;
+      font-size            : 1.2em;
+      color                : ${theme.white};
+      transition           : 2s;
+    }
+  b {
+    color                 : pink;
+  }
+  `
+  function template () {
+    return yo`
+      <div onmouseover=${hoverPortfolio}>
+        <div class="${css.portfolio}">
+          <div class="${css.portfolioItem}">
+            <div class="${css.portfolioTitle}">
+              Portfolio: My quiz app
+            </div>
+            <div class="${css.portfolioBody}">
+              My quiz is a quiz app where users can answer
+              questions about the<b>eSova project</b>and compare their answers
+              with others. It stores all the data in the database
+              and enables an admin view of all the answers.
+             </div>
+          </div>
+        </div>
+      </div>
+    `
+  }
+  function hoverPortfolio () {
+    var newElement = yo`
+      <div onmouseout=${unhoverPortfolio} onclick=${openNewTab}>
+        <div class="${css.portfolio}">
+          <div class="${css.portfolioItem_isHover}">
+            <div class="${css.portfolioTitle_isHover}">
+              Portfolio: My quiz app
+            </div>
+            <div class="${css.portfolioBody_isHover}">
+              My quiz is a quiz app where users can answer
+               questions about the <b>eSova project</b> and compare their answers
+              with others. It stores all the data in the database
+              and enables an admin view of all the answers.
+             </div>
+          </div>
+        </div>
+      </div>
+    `
+    yo.update(element, newElement)
+  }
+  function unhoverPortfolio() { yo.update(element, template()) }
+  function openNewTab() {
+    var url = "https://Cecana.github.io/Quiz/"
+    var tab = window.open(url, '_blank')
+    tab.focus()
+  }
+  var element = template()
+  return element
+}
+/*--------------------------------------------------------------------------------
+  TEXTBOX COMPONENT
+--------------------------------------------------------------------------------*/
+function textboxComponent ({ data = {}, theme = {} } = {}) {
+  var css = csjs`
+  .textbox {
+    margin: 5em 25% 3em 25%;
+    color: ${theme.grey};
+    font-size: 2em;
+    line-height: 1.5em;
+    text-align: justify;
+  }
+  `
+  function template () {
+    return yo`
+      <div>
+        <div class="${css.textbox}">
+          Let's keep in touch :)
+        </div>
+       </div>
+    `
+  }
+  var element = template()
+  return element
+}
+
+
+/*--------------------------------------------------------------------------------
+  FOOTER COMPONENT
+--------------------------------------------------------------------------------*/
+
+function footerComponent ({ data = {}, theme = {} } = {}) {
+	var css = csjs`
+  	.container {
+      display: flex;
+      justify-content: center;
+    }
+    .icon {
+      padding: 1em;
+      font-size: 35px;
+      color: ${theme.grey};
+    }
+    .icon:hover {
+      opacity: 0.4;
+    }
+  `
+  function template () {
+    return yo`
+    <div class="${css.container}">
+      <a href="https://github.com/Cecana">
+        <i class="${css.icon} fa fa-github" aria-hidden="true"></i>
+      </a>
+        <a href="mailto:cecanam@gmail.com ">
+        <i class="${css.icon} fa fa-envelope-o" aria-hidden="true"></i>
+      </a>
+      <a href="https://www.facebook.com/svetlana.milutinovic.5">
+       <i class="${css.icon} fa fa-facebook" aria-hidden="true"></i>
+      </a>
+       <a href="https://www.linkedin.com/in/svetlana-milutinovi%C4%87-655a6068?trk=nav_responsive_tab_profile">
+         <i class="${css.icon} fa fa-linkedin" aria-hidden="true"></i>
+         </a>
+      </div>
+
+    `
+  }
+  var element = template()
+  return element
+}
+/*-----------------------------------------------------------------------------
+  HELPERS
+-----------------------------------------------------------------------------*/
+function activateScrollEffect ({ colors = [] } = {}) {
+  var docHeight = (document.height !== undefined) ?
+    document.height
+  	: document.body.offsetHeight
+	var step = docHeight/colors.length
+  window.addEventListener("scroll", function(event) {
+  	var position = document.documentElement.scrollTop || document.body.scrollTop
+    var i = Math.floor(position/step)
+    var currentColor = colors[i]
+    document.body.style.backgroundColor = currentColor
+    document.body.style.transition = "background-color 3s"
+  })
+}
